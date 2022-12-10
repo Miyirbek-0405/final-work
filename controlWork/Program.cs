63c47dbf37db30bd7978af60f8e35b661e5c7770
Miyirbek <miyirbekmaxsetuli@gmail.com>
2022-12-11 04:35:32 +0500

    делал оформление коду

--- controlWork/Program.cs
+++ controlWork/Program.cs
@@ -1,12 +1,16 @@
-﻿string[] array={"misha","nasha","-)","off","ti"};
+﻿string[] array = { "misha", "nasha", "-)", "off", "ti" };
 
-void CreateNewArray(string [] arr){
-    for(int i=0;i<arr.Length;i++){
-        int cout=0;
-        for(int j=0;j<arr[i].Length;j++){
+void CreateNewArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int cout = 0;
+        for (int j = 0; j < arr[i].Length; j++)
+        {
             cout++;
         }
-        if(cout<=3){
+        if (cout <= 3)
+        {
             Console.Write($"{arr[i]}, ");
         }
     }
