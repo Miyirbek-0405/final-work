a59b36cf7f1ed2cb0ac1f34179ae19c47c7c8355
Miyirbek <miyirbekmaxsetuli@gmail.com>
2022-12-11 04:32:17 +0500

    решение задач

---
+++ controlWork/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array={"misha","nasha","-)","off","ti"};
+
+void CreateNewArray(string [] arr){
+    for(int i=0;i<arr.Length;i++){
+        int cout=0;
+        for(int j=0;j<arr[i].Length;j++){
+            cout++;
+        }
+        if(cout<=3){
+            Console.Write($"{arr[i]}, ");
+        }
+    }
+}
+CreateNewArray(array);
